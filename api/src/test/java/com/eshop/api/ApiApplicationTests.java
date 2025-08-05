15d16b28788644e3740b42b9ce52311a162bf8ad
SARR <sarrs490@gmail.com>
2025-08-05 10:16:02 +0000

    INITIAL COMMIT/ Service Eureka + Gateway

---
+++ api/src/test/java/com/eshop/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshop.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
